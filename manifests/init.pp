f12aaa65b896d53f0a7f233a3f7e3d96e5d2bbdc
danielwindit <daniel@windit.nl>
2017-01-17 11:54:34 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,4 @@
-#
 class motd inherits motd::params {
-  case $::osfamily {
-    'redhat': {
       file { '/etc/motd':
         ensure  => file,
         owner   => 'root',
@@ -9,14 +6,5 @@ class motd inherits motd::params {
         mode    => '0644',
         content => template('motd/motd.erb'),
       }
-
-    } # redhat
-
-    default: {
-      fail("${title}, ${::osfamily} is an unknown OS family")
-    }
-
-  } # case
-
-} # end motd
+} 
 
